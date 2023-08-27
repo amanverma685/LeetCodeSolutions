c360cf53d3f59dce897a195c9e0fc8c7fa834f31
Aman Kumar Verma <av36232@gmail.com>
2023-08-27 15:04:02 +0530

    Time: 22 ms (81.57%) | Memory: 16.8 MB (23.22%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-    void solve(vector<int>&vis,vector<int>adj[],int index)
+    void solve(vector<int>adj[],vector<int>&vis,int i)
     {
-        vis[index]=1;
-        for(auto node : adj[index])
+        vis[i]=1;
+        for(auto it:adj[i])
         {
-            if(!vis[node])
+            if(vis[it]==0)
             {
-                solve(vis,adj,node);
+                solve(adj,vis,it);
             }
         }
-
+        return;
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        int m = isConnected[0].size();
+    int findCircleNum(vector<vector<int>>& arr) {
+        int  n = arr.size();
+        int ans =0;
 
         vector<int>vis(n,0);
         vector<int>adj[n];
 
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
-                if(isConnected[i][j]==1 and i != j)
+                if(i==j)
+                continue;
+
+                if(arr[i][j]==1)
                 {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
-                
             }
         }
-        int j=0;
         for(int i=0;i<n;i++)
         {
             if(vis[i]==0)
             {
-                solve(vis,adj,i);
-                j++;
+                solve(adj,vis,i);
+                ans++;
             }
         }
-        return j;
+        return ans;
     }
 };

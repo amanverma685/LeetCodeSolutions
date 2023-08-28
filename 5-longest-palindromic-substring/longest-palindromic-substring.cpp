c65337377256158433acd60ba9ffcc11854357ae
Aman Kumar Verma <av36232@gmail.com>
2023-08-28 17:06:26 +0530

    Time: 1116 ms (10.83%) | Memory: 283.9 MB (12.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    int solve(const string &s,vector<vector<int>> &dp, int i, int j){
-    
-    if(i==j || i>j) return 1;   
-    if(dp[i][j]!=-1)return dp[i][j];
-    
-    if(s[i]==s[j]) dp[i][j]=solve(s,dp,i+1,j-1);
-    else dp[i][j]=0;
-    
-    solve(s,dp,i+1,j);
-    solve(s,dp,i,j-1);
-  
-    return dp[i][j];  
-}
+    int solve(string &str,int i,int j,vector<vector<int>>&dp)
+    {
+        if(i==j or i>j)
+        return 1;
+        if(dp[i][j]!= -1)
+        return dp[i][j];
 
-string longestPalindrome(string s) {
-    
-    int n=s.length();
-    vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        if(str[i]==str[j])
+        dp[i][j]=solve(str,i+1,j-1,dp);
 
-    solve(s,dp,0,n-1);
-    int x=0,y=0;
-    
-    for(int i=0; i<=n; i++)
-        for(int j=i+1; j<=n; j++)
-            if(dp[i][j]==1 && j-i>y-x) x=i,y=j;
-    
-    return s.substr(x,y-x+1);
-}
+        else dp[i][j]=0;
+        solve(str,i+1,j,dp);
+        solve(str,i,j-1,dp);
+        return dp[i][j];
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        solve(s,0,n-1,dp);
+        int x=0,y=0;
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j= i+1;j<=n;j++)
+            {
+                if(dp[i][j]==1 and j-i >y-x)
+                x=i,y=j;
+            }
+        }
+        return s.substr(x,y-x+1);
+    }
 };

2787e2f33891c5e5d7f279708cebcdc20e3ea62c
Aman Kumar Verma <av36232@gmail.com>
2023-08-28 18:29:50 +0530

    Time: 11 ms (88.43%) | Memory: 19.3 MB (47.08%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int solve(string &str,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i==j or i>j)
-        return 1;
-        if(dp[i][j]!= -1)
-        return dp[i][j];
-
-        if(str[i]==str[j])
-        dp[i][j]=solve(str,i+1,j-1,dp);
-
-        else dp[i][j]=0;
-        solve(str,i+1,j,dp);
-        solve(str,i,j-1,dp);
-        return dp[i][j];
-    }
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        solve(s,0,n-1,dp);
-        int x=0,y=0;
-
-        for(int i=0;i<=n;i++)
+        int currLen = 0;
+        string ans = "";
+        for(int i=0;i<n;i++)
         {
-            for(int j= i+1;j<=n;j++)
+            int left =i,right = i;
+            while(left>=0 and right<n and s[left]==s[right])
             {
-                if(dp[i][j]==1 and j-i >y-x)
-                x=i,y=j;
+                if(right-left + 1>currLen)
+                {
+                    currLen = right-left +1;
+                    ans = s.substr(left,right-left+1);
+                }   
+                left--;
+                right++;
+            }  
+            left = i;
+            right = i+1;
+            while(left>=0 and right<n and s[left]==s[right])
+            {
+                if(currLen<right-left+1)
+                {
+                    currLen = right-left+1;
+                    ans = s.substr(left,right-left+1);
+                }
+                left--;
+                right++;
+
             }
+
         }
-        return s.substr(x,y-x+1);
+        return ans;
     }
 };
